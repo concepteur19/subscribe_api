bdeb9b0bf39a53fab0a22d70ba41ce483e62572c
zobel ulrich Nguening Tchomgui <78241835+concepteur19@users.noreply.github.com>
2024-05-15 16:02:19 +0200

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -69,8 +69,7 @@ return [
     |
     */
 
-    'links' => [
-        public_path('articles') => storage_path('app/articles'),
+     'links' => [
+        public_path('storage') => storage_path('app/public'),
     ],
-
 ];

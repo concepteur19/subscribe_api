e06f629cc0281cfbc6df60064584cd51f93c86d4
ChristianLudovic <talchrist10@gmail.com>
2024-05-18 12:03:40 +0300

    "fix : some cors.php value was changed"

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/home', function () {
     return view('welcome');
+});
+
+Route::get('/authenticating', function () {
+    return view('authenticating');
 });
